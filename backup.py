e1f5fab8cf0630f1518d10f7c2f36f2b86fdafd9
Jan <jan@wiseserve.net>
2023-11-14 14:17:36 +0000

    added option to list and pick directories

--- backup.py
+++ backup.py
@@ -1,17 +1,51 @@
+import os
 import subprocess
 
+def choose_directory(prompt):
+    while True:
+        path = input(prompt)
+        if os.path.isdir(path):
+            return path
+        else:
+            print("Invalid directory. Please try again.")
+
+def list_directories(path):
+    try:
+        dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+        for i, d in enumerate(dirs, start=1):
+            print(f"{i}) {d}")
+        return dirs
+    except FileNotFoundError:
+        print("Directory not found.")
+        return []
+
+def select_directories():
+    base_path = choose_directory("Enter the base path to list directories: ")
+    dirs = list_directories(base_path)
+    if not dirs:
+        return []
+    selected = input("Enter the numbers of the directories to back up (comma-separated): ")
+    selected_indices = [int(i.strip()) - 1 for i in selected.split(',') if i.strip().isdigit()]
+    return [os.path.join(base_path, dirs[i]) for i in selected_indices if 0 <= i < len(dirs)]
+
 def local_backup():
-    dirs = input("Enter the directory or directories to back up (space-separated): ")
-    dest = input("Enter the destination directory: ")
+    dirs = select_directories()
+    if not dirs:
+        print("No directories selected for backup.")
+        return
+    dest = choose_directory("Enter the destination directory: ")
     print("Starting local backup...")
-    subprocess.run(["rsync", "-avh", "--progress"] + dirs.split() + [dest])
+    subprocess.run(["rsync", "-avh", "--progress"] + dirs + [dest])
     print("Local backup completed.")
 
 def ssh_backup():
-    dirs = input("Enter the directory or directories to back up (space-separated): ")
+    dirs = select_directories()
+    if not dirs:
+        print("No directories selected for backup.")
+        return
     ssh_dest = input("Enter SSH destination (user@host): ")
-    dest = input("Enter the destination directory on the remote host: ")
+    dest = choose_directory("Enter the destination directory on the remote host: ")
     ssh_key = input("Enter the path to your private SSH key (e.g., ~/.ssh/id_rsa): ")
     print("Starting backup over SSH...")
-    subprocess.run(["rsync", "-avh", "--progress", "-e", f"ssh -i {ssh_key}"] + dirs.split() + [f"{ssh_dest}:{dest}"])
+    subprocess.run(["rsync", "-avh", "--progress", "-e", f"ssh -i {ssh_key}"] + dirs + [f"{ssh_dest}:{dest}"])
     print("SSH backup completed.")
